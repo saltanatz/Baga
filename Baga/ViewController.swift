059631fc0d2e94f998bc59d13cf3dfb155b1c024
Salta <zarkhinovasalta@icould.com>
2023-12-11 18:05:43 +0600

    Initial Commit

---
+++ Baga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baga
+//
+//  Created by Saltanat on 11.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
